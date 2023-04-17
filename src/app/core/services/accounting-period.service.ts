475e22fc2a570d3e7ae36db5c32d533bcc8762ac
Michał Lewandowski <michal.lewandowski99@gmail.com>
2023-04-17 21:04:02 +0200

    removed unused import

--- src/app/core/services/accounting-period.service.ts
+++ src/app/core/services/accounting-period.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { AccountingPeriod } from '@shared/models/period.model';
-import * as dayjs from 'dayjs';
 import { Observable } from 'rxjs';
 
 @Injectable({
